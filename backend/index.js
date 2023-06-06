f58590b432392eec5d35d48a4b1064c894948b6b
Nestor Vega <vega.nestor1@gmail.com>
2023-06-05 21:34:07 -0700

    fixed connection to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./routes/products');
+require('dotenv').config();
+
 
 // Use the product routes
 app.use('/api/products', productRoutes);
@@ -12,16 +14,15 @@ app.use(express.json());
 // Set up your MongoDB connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-    .then(()=>{
-        console.log('Connected to MongoDB')
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
     })
-    .catch(()=>{
-        console.log('Error connecting to MongoDB')
-    })
-
+    .catch((error) => {
+      console.log('Error connecting to MongoDB:', error);
+    });
 
 // Start the server
 const port = process.env.PORT || 5000;
