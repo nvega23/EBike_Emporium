704ce538e5ac3271f5cd49f0abb94468f3011f67
Nestor Vega <vega.nestor1@gmail.com>
2023-06-11 12:51:39 -0700

    fixed paths for models

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
 require('./models/Users');
-require('./models/Passport');
+require('./models/Product');
 require('./config/passport'); // <-- ADD THIS LINE
 const passport = require('passport'); // <-- ADD THIS LINE
 
